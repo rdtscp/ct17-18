d14995120ac840fd2b83a775eb8fdf41dc6ba1ba
s1421803 <s1421803@Alexanders-MacBook-Pro.local>
2017-10-04 11:04:35 +0100

    Updated test case

--- tests/parser/test_0.c
+++ tests/parser/test_0.c
@@ -2,6 +2,15 @@ struct foo {
     char x;
     int y;
     char x[5];
+    struct foo mystruct[5];
 };
 
 struct foo mystruct;
+
+void main() {
+    int x;
+    y = (int)('c');
+    y = (int)("string");
+    y = (int)(5);
+    x = (int)foo.x;
+}
