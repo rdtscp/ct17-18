d3e77a17321cbc4ef4aab5333aaceafb6c146cdf
Christophe Dubach <christophe.dubach@ed.ac.uk>
2017-10-12 18:15:09 +0100

    Added description of part II + updated syntax grammar (unary minux operator)

--- src/parser/Parser.java
+++ src/parser/Parser.java
@@ -1,10 +1,15 @@
 package parser;
 
+import ast.FunDecl;
+import ast.Program;
+import ast.StructTypeDecl;
+import ast.VarDecl;
 import lexer.Token;
 import lexer.Tokeniser;
 import lexer.Token.TokenClass;
 
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 
 
@@ -26,11 +31,11 @@ public class Parser {
         this.tokeniser = tokeniser;
     }
 
-    public void parse() {
+    public Program parse() {
         // get the first token
         nextToken();
 
-        parseProgram();
+        return parseProgram();
     }
 
     public int getErrorCount() {
@@ -120,34 +125,38 @@ public class Parser {
     }
 
 
-    private void parseProgram() {
+    private Program parseProgram() {
         parseIncludes();
-        parseStructDecls();
-        parseVarDecls();
-        parseFunDecls();
+        List<StructTypeDecl> stds = parseStructDecls();
+        List<VarDecl> vds = parseVarDecls();
+        List<FunDecl> fds = parseFunDecls();
         expect(TokenClass.EOF);
+        return new Program(stds, vds, fds);
     }
 
     // includes are ignored, so does not need to return an AST node
     private void parseIncludes() {
-	    if (accept(TokenClass.INCLUDE)) {
+        if (accept(TokenClass.INCLUDE)) {
             nextToken();
             expect(TokenClass.STRING_LITERAL);
             parseIncludes();
         }
     }
 
-    private void parseStructDecls() {
+    private List<StructTypeDecl> parseStructDecls() {
         // to be completed ...
+        return null;
     }
 
-    private void parseVarDecls() {
+    private List<VarDecl> parseVarDecls() {
         // to be completed ...
+        return null;
     }
 
-    private void parseFunDecls() {
+    private List<FunDecl> parseFunDecls() {
         // to be completed ...
+        return null;
     }
 
-    // to be completed ...        
+    // to be completed ...
 }
