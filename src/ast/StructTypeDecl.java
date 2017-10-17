18921493e7a2cc948a10b4b72a6164dd26399282
s1421803 <s1421803@Alexanders-MacBook-Pro.local>
2017-10-17 18:08:12 +0100

    StructTypeDecl AST Node created.

--- src/ast/StructTypeDecl.java
+++ src/ast/StructTypeDecl.java
@@ -1,10 +1,19 @@
 package ast;
 
+import java.util.ArrayList;
+
 /**
  * @author cdubach
  */
 public class StructTypeDecl implements ASTNode {
 
+    public final String structName;
+    public final ArrayList<VarDecl> varDecls;
+
+    public StructTypeDecl(String structName, ArrayList<VarDecl> varDecls) {
+        this.structName = structName;
+        this.varDecls   = varDecls;
+    }
 
     // to be completed
 
