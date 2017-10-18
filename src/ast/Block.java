c26acc8897ccf984c7661eec94989df415bbf6b0
s1421803 <s1421803@Alexanders-MacBook-Pro.local>
2017-10-18 19:43:36 +0100

    Block class

--- src/ast/Block.java
+++ src/ast/Block.java
@@ -1,8 +1,16 @@
 package ast;
 
+import java.util.List;
+
 public class Block extends Stmt {
 
-    // to complete ...
+    public final List<VarDecl> varDecls;
+    public final List<Stmt> stmts;
+
+    public Block(List<VarDecl> varDecls, List<Stmt> stmts) {
+        this.varDecls = varDecls;
+        this.stmts    = stmts;
+    }
 
     public <T> T accept(ASTVisitor<T> v) {
 	    return v.visitBlock(this);
