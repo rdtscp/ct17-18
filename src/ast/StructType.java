e49822cafaf64c552430a9890eafa38f02c00c32
s1421803 <s1421803@Alexanders-MacBook-Pro.local>
2017-10-18 14:12:30 +0100

    Refactored StructType

--- src/ast/StructType.java
+++ src/ast/StructType.java
@@ -3,11 +3,9 @@ package ast;
 public class StructType implements Type {
 
     public final String structType;
-    public final String structName;
 
-    public StructType(String structType, String structName) {
+    public StructType(String structType) {
         this.structType = structType;
-        this.structName = structName;
     }
 
     public <T> T accept(ASTVisitor<T> v) {
