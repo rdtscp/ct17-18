a82b85ebd29699bbb619118e547f529b5a72292d
s1421803 <s1421803@Alexanders-MacBook-Pro.local>
2017-10-22 17:15:38 +0100

    Exit semantic pass early if errors

--- src/sem/SemanticAnalyzer.java
+++ src/sem/SemanticAnalyzer.java
@@ -15,6 +15,7 @@ public class SemanticAnalyzer {
 		
 		// Apply each visitor to the AST
 		for (SemanticVisitor v : visitors) {
+			if (errors > 0) break;
 			prog.accept(v);
 			errors += v.getErrorCount();
 		}
