1601f92bbf872484480ccdb9bc818fe00557ef82
s1421803 <s1421803@Alexanders-MacBook-Pro.local>
2017-10-23 23:18:33 +0100

    Scope unit test

---
+++ tests/sem/scope_test.c
@@ -0,0 +1,18 @@
+int num;
+char ltr;
+
+void main() {
+    num = 5;
+    ltr = 'a';
+    if (num < 5) {
+        char ltr;
+    }
+
+    if (num < 5) num++;
+
+    while (num < 5) {
+        char ltr;
+    }
+
+    while (num < 5) char ltr;
+}
