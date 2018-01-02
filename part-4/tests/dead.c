72aeab3c400a246e850d8468380bc8a52eebd9ed
s1421803 <s1421803@Alexanders-MacBook-Pro.local>
2018-01-02 20:59:24 +0000

    Dead code test

--- part-4/tests/dead.c
+++ part-4/tests/dead.c
@@ -1,11 +1,15 @@
 int foo() {
   int a = 7;
+  
+  char x = 'a';    // dead
+  int y = 11;      // dead
+  int z = 12;      // dead
+
   int b = a * 2;
   int c = b - a;   // dead 
   int d = c / a;   // dead
-  return b;
-}
+  int e = y + z;   // dead
+  b = 5 + e;
 
-int main() {
-    return foo();
+  return b;
 }
