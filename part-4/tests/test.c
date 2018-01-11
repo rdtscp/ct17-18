52d7fa98aed19ec492583b1e74bd2dd915e8e828
s1421803 <s1421803@Alexanders-MacBook-Pro.local>
2018-01-11 10:20:03 +0000

    Test cases

---
+++ part-4/tests/test.c
@@ -0,0 +1,6 @@
+int main() {
+  int b = 4 * 2;
+  int c=b-7;
+  int x=c/5;
+  return b;
+}
---
+++ part-4/tests/test1.c
@@ -0,0 +1,6 @@
+// #include <stdio.h>
+int main(){
+    int dividend;
+    scanf("%d", &dividend);
+    return dividend;
+}
